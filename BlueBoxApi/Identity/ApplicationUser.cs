a3f688f12b8354130effa910a2c75549578a6feb
Dennis Garcia <BlueBoxDev@outlook.com>
2022-09-04 16:09:25 -0500

    Added Display Name

--- BlueBoxApi/Identity/ApplicationUser.cs
+++ BlueBoxApi/Identity/ApplicationUser.cs
@@ -4,5 +4,9 @@ namespace BlueBoxApi.Identity
 {
     public class ApplicationUser : IdentityUser
     {
+        /// <summary>
+        /// Display Name
+        /// </summary>
+        public string DisplayName { get; set; } = default!;
     }
 }
--- BlueBoxApi/Identity/Migrations/20220901020133_AddDisplayName.Designer.cs
+++ BlueBoxApi/Identity/Migrations/20220901020133_AddDisplayName.Designer.cs
@@ -4,6 +4,7 @@ using BlueBoxApi.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlueBoxApi.Identity.Migrations
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220901020133_AddDisplayName")]
+    partial class AddDisplayName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
